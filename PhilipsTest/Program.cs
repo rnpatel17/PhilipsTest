e65b1c3a075f66db3c7151886986f0e3879891ac
rnpatel17 <ripal_1984@ymail.com>
2017-09-28 16:12:49 -0400

    i added project

---
+++ PhilipsTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using Q42.HueApi;
+using Q42.HueApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhilipsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = Bridge();
+        }
+        public static async Task<bool> Bridge()
+        {
+            IBridgeLocator locator = new HttpBridgeLocator();
+
+            //var  bridgeIPs = await locator.LocateBridgesAsync(TimeSpan.FromSeconds(5));
+
+            ILocalHueClient client = new LocalHueClient("192.168.20.106");
+            client.Initialize("001788fffe7b215e");
+            var lights = client.GetLightsAsync();//://blog.falafel.com/winiot-philips-hue-touchscreen-app/();// ("1");
+           // v
+            
+            return true;
+        }
+    }
+}
---
+++ PhilipsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PhilipsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PhilipsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bd0d2fa4-20bf-4932-be36-a5382c36437f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
